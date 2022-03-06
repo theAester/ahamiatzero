4abbd7f16ba1fb05c72641396e0b0f1d5ef10221
= <=>
2022-03-06 14:39:49 +0330

    add files

---
+++ code.py
@@ -0,0 +1,1 @@
+print("ahamiat=0")
---
+++ code.s
@@ -0,0 +1,18 @@
+.section .data
+txt:	.ascii "ahamiat=0\n"
+.equ len, . - txt
+.section .text
+.globl _start
+
+_start:
+pushq %rbp
+movq $1, %rax
+movq $len, %rdx
+movq $1, %rdi
+movq $txt, %rsi
+syscall
+
+popq %rbp
+movq $12, %rdi
+movq $60, %rax
+syscall
